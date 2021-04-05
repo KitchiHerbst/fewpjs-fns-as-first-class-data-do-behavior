64bd4ddae5fe12f008bda5aab9a2a1b3e0351f5a
Triston <tristonherbst97@gmail.com>
2021-04-05 16:33:41 -0500

    commit tree 405b0caebd3a998c069d4896a83aff5b0cbb15bf

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(str) {
+  let time = str.split(':').join('')  
+  time = parseInt(time);
+  if (time < 1200) {
+    return "Good Morning";
+  }
+  else if (time >= 1200 && time < 1700) {
+    return 'Good Afternoon';
+  }
+  else  {
+    return 'Good Evening';
+  }
+}
+
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string
+  
+}
